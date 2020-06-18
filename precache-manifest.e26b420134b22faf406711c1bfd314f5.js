dd7a41909e40a5a6f0a573918d213158a5c3565e
Carlos Martins Rodrigues <Carlos_Martins_Rodri@Dell.com>
2020-06-18 13:17:45 +0100

    Updates

---
+++ precache-manifest.e26b420134b22faf406711c1bfd314f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b264a2d912d0f09c9fb69749391d0e",
+    "url": "/teenturn-sample-project/index.html"
+  },
+  {
+    "revision": "4ee828503c183b2e1d8f",
+    "url": "/teenturn-sample-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bdfa9cc5fc135572a9c9",
+    "url": "/teenturn-sample-project/static/js/2.bca02c52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/teenturn-sample-project/static/js/2.bca02c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee828503c183b2e1d8f",
+    "url": "/teenturn-sample-project/static/js/main.48d99310.chunk.js"
+  },
+  {
+    "revision": "db13309f9ce12ed6bbc8",
+    "url": "/teenturn-sample-project/static/js/runtime-main.5d5dee47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/teenturn-sample-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
